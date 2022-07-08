d221db94e8758c5e2a93bcce6525eae6abd9300b
SuprimDevkota <suprimdevkota@gmail.com>
2022-07-08 21:28:40 +0545

    Basics of Network Programming

--- Server/Basics/Accepting_client_connections.cpp
+++ Server/Basics/Accepting_client_connections.cpp
@@ -5,7 +5,12 @@ using namespace boost;
 
 int main()
 {
-	//The size of the queue containing the pending connection requests.
+	//The argument that the listen() method accepts as an argument
+	//specifies the size of the queue maintained by the operating system
+	//to which it puts connection requests arriving from the clients. 
+	//The requests stay in the queue and are waiting for the server
+	//application to dequeue and process them. When the queue becomes
+	//full, the new connection requests are rejected by the operating system.
 	const int BACKLOG_SIZE = 30;
 
 	//Assume that the server application has already obtained the protocol port
@@ -24,13 +29,33 @@ int main()
 		//Binding the acceptor socket to the server endpoint
 		acceptor.bind(ep);
 
-		//Start listening for incoming connection requests.
+		//Start listening for incoming connection requests. This call switches
+		//the acceptor socket into the state in which it listens for incoming
+		//connection requests. Unless listen() is called, all connection
+		//requests arriving at the corresponding endpoint will be rejected
+		//by the operating system network software.
+
+		
 		acceptor.listen(BACKLOG_SIZE);
 
 		//Create an active socket
 		asio::ip::tcp::socket sock(ios);
 
-		//Processing the next connection request and connecting the active socket to the client
+		//Processing the next connection request and connecting the active socket
+		//to the client. Accepts an active socket as argument and performs several
+		//operations. First, it checks the queue associated with the acceptor socket
+		//containind pending connection requests. If the queue is empty, the method
+		//blocks execution until a new connection request arrives to an endpoint to
+		//which the acceptor socket is bound and the operating system puts in in the queue
+
+		//If at least one connection request is available in the queue, the one on top of
+		//the queue is extracted from it and processed. The active socket that was passed to
+		//the accept() method as an argument is connected to the corresponding client application
+		//which issued the connection request
+
+		//If the connection establishment process succeeds, the accept() method returns
+		//and the active socket is opened and connected to the client application and can
+		//be used to send data to and recieve data from it.
 		acceptor.accept(sock);
 
 		//At this point sock scoket is connected to the client application
